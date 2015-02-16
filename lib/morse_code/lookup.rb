6440f89e6288734311df35b7c0957ac628573b77
Rob Ruff <robruff@gmail.com>
2015-02-15 21:26:33 -0500

    Added an extension module that adds an invert_with_mods method.

--- lib/morse_code/lookup.rb
+++ lib/morse_code/lookup.rb
@@ -1,17 +1,37 @@
 module MorseCode
+  # Extend a hash-like object so we can invert it
+  # and modify the key/value entries at the same time.
+  # Used internally.
+  module ExtHash
+    # Invert the hash. The keys and values can be modified
+    # by providing a block.
+    def invert_with_mods
+      unless self.respond_to?(:invert)
+        raise TypeError, "Expected a Hash or Hash-like object: #{self.inspect}"
+      end
+    
+      self.invert unless block_given?
+    
+      new_hash = {}
+      self.each do |k, v|
+        nk, nv = yield(k, v)
+        new_hash[nk] = nv
+      end
+    
+      new_hash.invert
+    end
+  end
+  
+  ##
+  # TODO: Generalize this to a bidirectional Hash class
+  ##
+  
   # A lookup table mapping Morse Code sequences to corresponding
   # alphanueric characters and vice versa.
   class Lookup
     class << self
       attr_reader :char_to_seq, :seq_to_char
     end
-    
-    def self.create_seq_map(char_map)
-      seq_map = Hash.new
-      char_map.each { |k, v| seq_map[v.join.to_sym] = k.to_s.freeze }
-      seq_map
-    end
-    private_class_method :create_seq_map
     
     @char_to_seq = {
       :A => %w(. -).freeze,
@@ -50,9 +70,11 @@ module MorseCode
       :"8" => %w(- - - . .).freeze,
       :"9" => %w(- - - - .).freeze,
       :"0" => %w(- - - - -).freeze
-    }.freeze
+    }.extend(ExtHash).freeze
     
-    @seq_to_char = create_seq_map(@char_to_seq).freeze
+    # seq_to_char is an inversion of char_to_seq. The sequence values are transformed to
+    # symbols and become the new keys. E.g., :".-" => "A"
+    @seq_to_char = @char_to_seq.invert_with_mods { |k, v| [k.to_s, v.join.to_sym] }
     
     # Accepts an alphanumeric character and returns the corresponding
     # sequence of Morse Code signals.
